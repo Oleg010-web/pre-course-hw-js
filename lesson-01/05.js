60879503c7827aae944e56314dfb679a1051eb3d
Oleg010-web <nesteroff_oleg@mail.ru>
2024-02-11 18:51:09 +0300

    05-task-completed

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,6 +1,6 @@
 // code
 let rule = "Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.";
-switch (rule.length) {
+switch (true) {
   case rule.length > 25:
     console.log("У меня 100 пудов все получится");
     break;
