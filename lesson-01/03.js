509763d7529750d4eda51354b5587bed650aaafd
Oleg010-web <nesteroff_oleg@mail.ru>
2024-02-11 16:59:05 +0300

    03-task-completed

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,6 +1,6 @@
 // code
 let number = 1;
-console.log(number++);
-console.log(++number);
-console.log(++number + number++ - number--);
-console.log(number - --number);
+console.log(number++); 
+console.log(++number); 
+console.log(number--); 
+console.log(--number); 
