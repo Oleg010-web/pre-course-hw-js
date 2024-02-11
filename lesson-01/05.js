2249cac778bcf1ce5b79994f5711e9b8ebe54855
Oleg010-web <nesteroff_oleg@mail.ru>
2024-02-11 18:50:23 +0300

    05-task-completed

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -5,9 +5,11 @@ switch (rule.length) {
     console.log("У меня 100 пудов все получится");
     break;
   case rule.length < 25:
-    console.log("Все таки нет правил без исключения")
+    console.log("Все таки нет правил без исключения");
     break;
   case rule.length == 25:
     console.log("50 на 50"); 
+    break;
+  default:console.log("50 на 50")
     break;
 }
