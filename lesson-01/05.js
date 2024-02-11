a983da4f6666c630e6e8f2a57e146d764314ec88
Oleg010-web <nesteroff_oleg@mail.ru>
2024-02-11 18:40:56 +0300

    05-task-completed

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -7,8 +7,7 @@ switch (rule.length) {
   case rule.length < 25:
     console.log("Все таки нет правил без исключения")
     break;
-
-  default: rule.length === 25
-    console.log("50 на 50");
+  case rule.length == 25:
+    console.log("50 на 50"); 
     break;
 }
