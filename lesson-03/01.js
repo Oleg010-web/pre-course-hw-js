cab092a5bf96f571acb67232e6d54863455589a7
Oleg010-web <nesteroff_oleg@mail.ru>
2024-02-28 16:54:28 +0300

    03-lesson-01-task-completed

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,16 @@
 // code
+let colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+let colorsOfGermany = [];
+
+function createColorString() {
+    colors.filter((item) => {
+      if (item === "черный" || item === "красный" || item === "желтый") {
+        colorsOfGermany.push(item);
+      }
+    })
+    
+    return colorsOfGermany.join('-');
+}
+
+console.log(createColorString());
+
